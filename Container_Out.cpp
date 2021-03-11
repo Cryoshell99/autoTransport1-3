37d41b57412142bad1bf0d089f71ab69fadd1423
Cryoshell <cryoshell1999@gmail.com>
2021-03-11 14:42:51 +0700

    Changed filtered output method

--- Container_Out.cpp
+++ Container_Out.cpp
@@ -4,6 +4,7 @@
 using namespace std;
 
 void Out(Transport* tr, ofstream& ofst);
+void OutBus(Transport* tr, ofstream& ofst);
 
 void Out(Container& cnt, ofstream& ofst)
 {
@@ -12,6 +13,30 @@ void Out(Container& cnt, ofstream& ofst)
 	{
 		ofst << "Container contains that elements:" << endl;
 		Container *temp;
+		temp = &cnt;
+		int i = 0;
+		do
+		{
+			ofst << i << ": ";
+			Out(temp->L, ofst);
+			temp = temp->Next;
+			i++;
+		} while (temp != &cnt);
+	}
+	else
+	{
+		ofst << "Container is empty!" << endl;
+	}
+};
+
+void OutBus(Container& cnt, ofstream& ofst)
+{
+	ofst << endl << "Only Bus transports:" << endl;
+	// Если контейнер не пуст
+	if (&cnt != nullptr)
+	{
+		ofst << "Container contains that elements:" << endl;
+		Container* temp;
 		temp = &cnt;
 		int i = 0;
 		do
--- autoTransport.cpp
+++ autoTransport.cpp
@@ -9,6 +9,7 @@ void Init(Container& cnt);
 void Clear(Container*& cnt);
 void In(Container& cnt, ifstream& ifst);
 void Out(Container& cnt, ofstream& ofst);
+void OutBus(Container& cnt, ofstream& ofst);
 
 int main(int argc, char* argv[])
 {
@@ -32,6 +33,8 @@ int main(int argc, char* argv[])
 	ofst << "Filled container. " << endl;
 	// Выводим содержимое
 	Out(*cnt, ofst);
+	// Фильтрованный вывод
+	OutBus(*cnt, ofst);
 	// Чистим его
 	Clear(cnt);
 	// Демностируем содержимое контейнера
